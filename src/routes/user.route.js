bba6d3931de86dd76b0e14d3a60774af724b01d4
Ankit Sharma <ankitsharma101299@gmail.com>
2022-09-06 08:34:51 +0530

    Add new route for login

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,25 +3,24 @@ const router = express.Router();
 
 const userController = require("../controllers/user.controller");
 
+
 // Temporary functions to keep track of user count in redis
 router.get('/redis_user_count', userController.getCurrentRedisCount);
 router.post('/redis_user_count', userController.incrementRedisCount);
-
 // ###### Temporary routes to keep track of user count in redis
-
 // Get current redis counter
 router.get("/getCurrentRedisCount", userController.getCurrentRedisCount);
-
 router.get("/setRedisCount", userController.setRedisCount);
-
 router.get("/incrementRedisCount", userController.incrementRedisCount);
+
+
+router.post('/login', userController.login);
+
 
 // Get a single user
 router.get("/:userId", userController.getUser);
-
 // Get the users data
 router.get("/", userController.getUsers);
-
 // Create a user
 router.post('/', userController.createUser);
 
